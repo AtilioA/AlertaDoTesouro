b9541a322fd34e23efe1f28e1e5522a93dc299fe
AtilioA <atiliodadalto@hotmail.com>
2020-08-15 18:37:53 -0300

    chore: add .vscode
    create attach debug configuration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333!");
+  console.log('Server started on port 3333!');
 });
