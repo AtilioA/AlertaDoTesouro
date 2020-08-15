802f718e801f39ebe1ce38792d03cac51017104b
AtilioA <atiliodadalto@hotmail.com>
2020-08-15 18:26:26 -0300

    chore: add prettier
    adapt eslint to prettier, add eslintignore

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello, World!" });
-});
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello, World!' })
+);
 
 export default routes;
