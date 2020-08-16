54c489b773ee3284e351e8501db1c4dd58fc4b54
AtilioA <atiliodadalto@hotmail.com>
2020-08-16 17:03:50 -0300

    refactor(server): separate server into server.ts and app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,4 @@
-import express from 'express';
-import routes from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
+import { app } from './app';
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
