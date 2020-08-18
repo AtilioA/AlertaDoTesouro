1bbbbfa4e78e67c5206245398b987a8c467a82eb
AtilioA <atiliodadalto@hotmail.com>
2020-08-17 22:44:46 -0300

    feat(server): add CreateNotifications migration

---
+++ src/database/migrations/1597711023289-CreateNotifications.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateNotifications1597711023289
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'notifications',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'bond',
+            type: 'json',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'float',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'notifyByEmail',
+            type: 'bool',
+            default: true,
+          },
+          {
+            name: 'notifyByBrowser',
+            type: 'bool',
+            default: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('notifications');
+  }
+}
