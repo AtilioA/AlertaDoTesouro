273d24c959b9826f52206ac62f84e5c4eb284eba
AtilioA <atiliodadalto@hotmail.com>
2020-08-19 11:27:59 -0300

    feat(server): create initial user model and migration

---
+++ src/database/migrations/1597845225214-CreateUsers.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1597845225214 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'notify',
+            type: 'bool',
+            default: true,
+          },
+          {
+            name: 'notifyByEmail',
+            type: 'bool',
+            default: true,
+          },
+          {
+            name: 'notifyByBrowser',
+            type: 'bool',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
---
+++ src/models/User.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  name: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column('boolean')
+  notify: boolean;
+
+  @Column('boolean')
+  notifyByEmail: boolean;
+
+  @Column('boolean')
+  notifyByBrowser: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
