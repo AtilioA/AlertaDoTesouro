8d95b09136fd687bba7fc35fbf9c26cea2191c17
AtilioA <atiliodadalto@hotmail.com>
2020-08-23 17:00:35 -0300

    feat(server): create task to update treasury bonds every 15 minutes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
 import 'reflect-metadata';
 import { app } from './app';
+var cron = require('node-cron');
 
 import './database';
+import UpdateTreasuryBondService from './services/UpdateTreasuryBondService';
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
+});
+
+// Update treasury bonds
+const updateTreasuryBond = new UpdateTreasuryBondService();
+cron.schedule('*/15 * * * *', () => {
+  console.log('Running update-all-treasury-bonds task every 15 minutes...');
+  try {
+    updateTreasuryBond.execute();
+    console.log('Updated treasury bonds entries in the database!');
+  } catch (err) {
+    console.log(err);
+  }
 });
