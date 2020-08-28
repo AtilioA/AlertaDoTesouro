574224d83b415f7f9d366f369a5aee4ba6aa3d8b
AtilioA <atiliodadalto@hotmail.com>
2020-08-28 14:10:35 -0300

    refactor(server): clean unnecessary lines from notify-bond-return

--- src/jobs/NotifyBondReturns.ts
+++ src/jobs/NotifyBondReturns.ts
@@ -24,11 +24,6 @@ class NotifyBondReturns {
       subject: `${notification.bond.name || 'Um título'} agora tem retorno ${
         notification.type
       } que ${notification.value}%!`,
-      text: `<h1>Alerta do Tesouro</h1>
-${notification.bond.name || 'Um título'} tem retorno ao ano ${
-        notification.type
-      } que ${notification.value}% agora!`,
-
       template: 'notify-bond-returns',
       context: {
         treasurybond: notification.bond,
