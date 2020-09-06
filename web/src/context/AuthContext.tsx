0a03b4c86331051889d59bddb5bca3792c319245
AtilioA <atiliodadalto@hotmail.com>
2020-09-06 12:35:19 -0300

    feat(web): create signOut function within AuthContext

--- web/src/context/AuthContext.tsx
+++ web/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from "react";
+import React, { createContext, useCallback, useState } from "react";
 import api from "../services/api";
 
 interface SignInCredentials {
@@ -7,24 +7,52 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  email: string;
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+interface AuthState {
+  token: string;
+  user: object;
 }
 
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData); // Do not use a default value for AuthContext
 
 export const AuthProvider: React.FC = ({children}) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@AlertaDoTesouro:token');
+    const user = localStorage.getItem('@AlertaDoTesouro:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  })
+
   const signIn = useCallback(async ({email, password}: SignInCredentials) => {
     const response = await api.post('sessions', {
       email,
       password
     });
 
-    console.log(response.data)
+    const { token, user } = response.data;
+    localStorage.setItem('@AlertaDoTesouro:token', token);
+    localStorage.setItem('@AlertaDoTesouro:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@AlertaDoTesouro:token');
+    localStorage.removeItem('@AlertaDoTesouro:user');
+
+    setData({} as AuthState);
   }, []);
 
   return (
-    <AuthContext.Provider value={{email: "a@a.com", signIn}}>
+    <AuthContext.Provider value={{user: data.user, signIn, signOut}}>
       {children}
     </AuthContext.Provider>
   )
