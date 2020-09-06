88b95c149adad3d12f5e000a648912f9711e68bc
AtilioA <atiliodadalto@hotmail.com>
2020-09-06 10:29:54 -0300

    refactor(web): validate Account page Form and tweak its style

--- web/src/pages/Account/index.tsx
+++ web/src/pages/Account/index.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 
 import { Container } from './styles';
 import Input from '../../components/Input';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup'
+import getValidationErrors from '../../utils/getValidationErrors';
+import { Form } from '@unform/web';
+import { FiUser, FiCheck, FiLock, FiPlus } from 'react-icons/fi';
 
-const Account: React.FC = () => (
+const Account: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email é obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().required('Informe sua senha'),
+        newPassword: Yup.string().oneOf([Yup.ref('confirmPassword')], 'Senhas não batem'),
+        confirmPassword: Yup.string().when(
+          'newPassword',
+          (newPassword: string, field: any) =>
+            newPassword
+              ? field
+                  .required('É necessário confirmar sua senha').min(8, 'Mínimo de 8 caracteres').oneOf([Yup.ref('newPassword')], 'Senhas não batem')
+              : field,
+        ),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
   <Container>
-    <form>
+    <Form ref={formRef} onSubmit={handleSubmit}>
       <div id="form-header">
         <h1>SUAS INFORMAÇÕES</h1>
       </div>
@@ -13,21 +50,22 @@ const Account: React.FC = () => (
       <div id="input-header">
         <h2>EMAIL</h2>
       </div>
-      <Input name="email" placeholder="alan@turing.com" />
+      <Input icon={FiUser} name="email" placeholder="alan@turing.com" />
 
       <div id="input-header">
         <h2>SENHA</h2>
       </div>
-      <Input name="password" type="password" placeholder="Sua senha atual" />
-      <Input name="newPassword" type="password" placeholder="Sua nova senha" />
-      <Input name="confirmPassword" type="password" placeholder="Confirmação de sua nova senha" />
+      <Input icon={FiLock} name="password" type="password" placeholder="Sua senha atual" />
+      <Input icon={FiPlus} name="newPassword" type="password" placeholder="Sua nova senha" />
+      <Input icon={FiCheck} name="confirmPassword" type="password" placeholder="Confirmação de sua nova senha" />
 
       <button type="submit">Atualizar dados</button>
       <button id="sair" type="submit">Sair</button>
       <button id="deletar-conta" type="submit">Deletar conta</button>
 
-    </form>
+    </Form>
   </Container>
-);
+  );
+}
 
 export default Account;
--- web/src/pages/Account/styles.ts
+++ web/src/pages/Account/styles.ts
@@ -8,8 +8,6 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  /* width: 100%; */
-  /* max-width: 700px; */
 
   form {
     margin: 80px;
@@ -27,36 +25,24 @@ export const Container = styled.div`
       /* padding-bottom: 50px; */
     }
 
+
     div#input-header {
       font-size: 20px;
       text-align: left;
       margin: 5px 5px 5px 14px;
     }
 
-    input {
-      margin-left: 5px;
-      padding: 10px;
-      width: 97%;
-      background: #e2e2e2;
-      border-radius: 4px;
-      border: 2px solid #e2e2e2;
-      & + input {
-        margin-top: 5px;
-      }
-    }
-
     button {
-      /* background-color: #3b9eff; */
       color: #ffffff;
 
-      margin: 10px;
+      margin-top: 10px;
       padding: 0 16px;
 
       border-radius: 4px;
       border: 0;
       box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.1);
 
-      width: 90%;
+      width: 100%;
       height: 42px;
 
       background: #3b9eff;
@@ -82,34 +68,6 @@ export const Container = styled.div`
           background: ${shade(0.1, '#FF1818')};
         }
       }
-    }
-
-    a {
-      color: #777;
-      text-decoration: none;
-      transition: color 0.2s;
-      &:hover {
-        color: ${shade(0.5, '#555')};
-      }
-    }
-  }
-
-  > a {
-    color: #3b9fff;
-    display: flex;
-    align-items: center;
-    margin-top: 24px;
-    text-decoration: none;
-
-    transition: color 0.2s;
-    &:hover {
-      color: ${shade(0.2, '#3b9fff')};
-    }
-
-    svg {
-      margin-right: 5px;
-      /* margin-top: auto; */
-      /* margin-bottom: auto; */
     }
   }
 `;
