b8f11e573847852f1e21a666602ff9de167b2d3e
AtilioA <atiliodadalto@hotmail.com>
2020-09-06 11:01:51 -0300

    feat(server): use cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
 import logRequests from './middlewares/requestsLogger';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(logRequests);
 app.use(express.json());
 app.use(routes);
