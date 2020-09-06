f8a8eec8a4b675e7486c5a4fe4bbda3f97b0b507
AtilioA <atiliodadalto@hotmail.com>
2020-09-06 16:09:28 -0300

    feat(web): animate Account page

--- web/src/pages/Account/index.tsx
+++ web/src/pages/Account/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useCallback } from 'react';
 
-import { Container } from './styles';
+import { Container, AnimationContainer } from './styles';
 import Input from '../../components/Input';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup'
@@ -44,28 +44,30 @@ const Account: React.FC = () => {
 
   return (
   <Container>
-    <Form ref={formRef} onSubmit={handleSubmit}>
-      <div id="form-header">
-        <h1>SUAS INFORMAÇÕES</h1>
-      </div>
+    <AnimationContainer>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <div id="form-header">
+          <h1>SUAS INFORMAÇÕES</h1>
+        </div>
 
-      <div id="input-header">
-        <h2>EMAIL</h2>
-      </div>
-      <Input icon={FiUser} name="email" placeholder="alan@turing.com" />
+        <div id="input-header">
+          <h2>EMAIL</h2>
+        </div>
+        <Input icon={FiUser} name="email" placeholder="alan@turing.com" />
 
-      <div id="input-header">
-        <h2>SENHA</h2>
-      </div>
-      <Input icon={FiLock} name="password" type="password" placeholder="Sua senha atual" />
-      <Input icon={FiPlus} name="newPassword" type="password" placeholder="Sua nova senha" />
-      <Input icon={FiCheck} name="confirmPassword" type="password" placeholder="Confirmação de sua nova senha" />
+        <div id="input-header">
+          <h2>SENHA</h2>
+        </div>
+        <Input icon={FiLock} name="password" type="password" placeholder="Sua senha atual" />
+        <Input icon={FiPlus} name="newPassword" type="password" placeholder="Sua nova senha" />
+        <Input icon={FiCheck} name="confirmPassword" type="password" placeholder="Confirmação de sua nova senha" />
 
-      <button type="submit">Atualizar dados</button>
-      <button id="sair" type="submit">Sair</button>
-      <button id="deletar-conta" type="submit">Deletar conta</button>
+        <button type="submit">Atualizar dados</button>
+        <button id="sair" type="submit">Sair</button>
+        <button id="deletar-conta" type="submit">Deletar conta</button>
 
-    </Form>
+      </Form>
+    </AnimationContainer>
   </Container>
   );
 }
--- web/src/pages/Account/styles.ts
+++ web/src/pages/Account/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 export const Container = styled.div`
@@ -71,3 +71,18 @@ export const Container = styled.div`
     }
   }
 `;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`
+
+export const AnimationContainer = styled.div`
+  animation: ${appearFromLeft} 1s;
+`
