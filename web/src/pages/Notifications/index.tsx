8dcd83b70999e2c022b2b9d4128cec79fa8abd20
AtilioA <atiliodadalto@hotmail.com>
2020-09-08 00:41:17 -0300

    feat(web): create and style Notifications page

---
+++ web/src/pages/Notifications/index.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import Toggle from 'react-toggle';
+
+import {
+  Container,
+  AnimationContainer,
+  Notification,
+  NotificationsContainer,
+  NotificationContainer,
+} from './styles';
+import { FiEdit, FiTrash } from 'react-icons/fi';
+// import Input from '../../components/Input';
+
+const Notifications: React.FC = () => {
+  function handleNotifyChange() {
+    return true;
+  }
+
+  return (
+    <AnimationContainer>
+      <Container>
+        <div id="header">
+          <h1>CONFIGURAÇÕES GLOBAIS</h1>
+        </div>
+
+        <div id="global-notification-settings">
+          <div id="toggle-with-label">
+            <span>Receber notificações</span>
+            <Toggle
+              id="notification-status"
+              defaultChecked={true}
+              onChange={() => handleNotifyChange()}
+            />
+          </div>
+          <div id="global-notification-settings-body">
+            <div id="toggle-with-label">
+              <span>Receber notificações por e-mail</span>
+              <Toggle
+                id="notification-status"
+                defaultChecked={true}
+                onChange={() => handleNotifyChange()}
+              />
+            </div>
+            <div id="toggle-with-label">
+              <span>Receber notificações pelo navegador</span>
+              <Toggle
+                id="notification-status"
+                defaultChecked={true}
+                onChange={() => handleNotifyChange()}
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+      <NotificationsContainer>
+        <div id="header">
+          <h1>NOTIFICAÇÕES</h1>
+        </div>
+        <NotificationContainer>
+          <Notification>
+            <div id="notification-content">
+              <div id="notification-bond">
+                <h1>Tesouro IPCA 2026+</h1>
+                <div id="notification-bond-value">
+                  <span>Taxa atual:</span>
+                  <b>XX.XX%</b>
+                </div>
+                <div id="notification-bond-value">
+                  <span>Preço unitário:</span>
+                  <b>R$XX.XX</b>
+                </div>
+              </div>
+              <div id="notification-summary">
+                <span>
+                  Você será notificado quando a taxa estiver <u>maior</u> que{' '}
+                  <b>XX.XX%</b>.
+                </span>
+                <span>Notificação criada em XX/XX/XXXX.</span>
+              </div>
+              <div id="notification-actions">
+                <div id="toggle-with-label">
+                  <h1>Ativa</h1>
+                  <Toggle
+                    id="notification-status"
+                    defaultChecked={true}
+                    onChange={() => handleNotifyChange()}
+                  />
+                </div>
+                <div id="notification-actions-active-minor">
+                  <div id="toggle-with-label">
+                    <span>E-mail</span>
+                    <Toggle
+                      id="notification-status"
+                      defaultChecked={true}
+                      onChange={() => handleNotifyChange()}
+                    />
+                  </div>
+                  <div id="toggle-with-label">
+                    <span>Browser</span>
+                    <Toggle
+                      id="notification-status"
+                      defaultChecked={true}
+                      onChange={() => handleNotifyChange()}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Notification>
+          <div id="notification-actions-edit-delete">
+            <div id="edit">
+              <button>
+                <FiEdit />
+              </button>
+            </div>
+            <div id="delete">
+              <button>
+                <FiTrash />
+              </button>
+            </div>
+          </div>
+        </NotificationContainer>
+      </NotificationsContainer>
+    </AnimationContainer>
+  );
+};
+
+export default Notifications;
---
+++ web/src/pages/Notifications/styles.ts
@@ -0,0 +1,247 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import '../../components/Toggle/styles.css';
+
+export const Container = styled.div`
+  display: flex;
+
+  margin-top: 30px;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    margin-bottom: 12px;
+  }
+
+  div#toggle-with-label {
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  div#global-notification-settings {
+    align-items: center;
+
+    span {
+      font-size: 24px;
+      margin: 2px 10px 2px 10px;
+    }
+
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+    flex-direction: column;
+    font-size: 20px;
+    text-align: left;
+    background: #e2e2e2;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 2px;
+  }
+
+  div#global-notification-settings-body {
+    span {
+      font-size: 20px;
+    }
+
+    div#toggle-with-label {
+      margin-top: 4px;
+    }
+
+    justify-content: space-around;
+    margin-top: 30px;
+  }
+
+  button {
+    color: #ffffff;
+
+    margin-top: 10px;
+    padding: 0 16px;
+
+    border-radius: 4px;
+    border: 0;
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.1);
+
+    width: 100%;
+    height: 42px;
+
+    background: #3b9eff;
+
+    text-align: center;
+    font-weight: bold;
+
+    transition: background 0.2s;
+    &:hover {
+      background: ${shade(0.1, '#3b9fff')};
+    }
+  }
+`;
+
+export const NotificationsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 75px;
+
+  h1 {
+    margin-bottom: 12px;
+  }
+`;
+
+export const NotificationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
+  div#notification-content {
+    display: flex;
+    flex-direction: row;
+  }
+
+  div#notification-actions-edit-delete {
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+  }
+
+  div#edit {
+    button {
+      background: #a7b6cb;
+      border: 0;
+      padding: 25px;
+      border-radius: 0px 2px 0px 0px;
+
+      transition: background 0.2s;
+      &:hover {
+        background: ${shade(-0.1, '#a7b6cb')};
+      }
+    }
+  }
+
+  div#delete {
+    button {
+      background: #cba7a7;
+      padding: 25px;
+      border: 0;
+      border-radius: 0px 0px 2px 0px;
+
+      transition: background 0.2s;
+      &:hover {
+        background: ${shade(-0.1, '#cba7a7')};
+      }
+    }
+  }
+`;
+
+export const Notification = styled.div`
+  max-width: 95%;
+  background: #e2e2e2;
+  border-radius: 2px;
+  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+  display: flex;
+  padding: 10px 0px 10px 0px;
+  flex-direction: row;
+
+  div#notification-content {
+    padding: 0px 20px 0px 20px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  div#notification-bond {
+    border-right: 1px solid #828282;
+    padding-right: 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    h1 {
+      font-size: 20px;
+      margin-bottom: 10px;
+    }
+
+    div#notification-bond-value {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      span {
+        font-weight: normal;
+        font-size: 16px;
+      }
+    }
+  }
+
+  div#notification-summary {
+    padding-left: 25px;
+    padding-right: 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    span {
+      & + span {
+        margin-top: 10px;
+      }
+    }
+  }
+
+  div#toggle-with-label {
+    span {
+      margin-right: 10px;
+    }
+
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 10px;
+  }
+
+  div#notification-actions-active-minor {
+    margin-top: 20px;
+
+    span {
+      margin-top: 8px;
+      & + div {
+        margin-top: 8px;
+      }
+    }
+  }
+
+  div#notification-actions {
+    padding-left: 20px;
+    border-left: 1px solid #828282;
+
+    h1 {
+      font-size: 20px;
+    }
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  div#notification-actions-edit-delete {
+    display: flex;
+    flex-direction: column;
+    border-radius: 0px 2px 0px 2px;
+  }
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  animation: ${appearFromLeft} 1s;
+`;
