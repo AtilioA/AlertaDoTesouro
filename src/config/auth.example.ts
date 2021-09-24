7f12bc144709cd65a6799275f0ec39d9a6e3e977
AtilioA <atiliogpg@protonmail.com>
2021-09-24 15:53:59 -0300

    fix(server): add missing examples for config files

---
+++ src/config/auth.example.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'verylongandrandomizedsecretstring',
+    expiresIn: '1d',
+  },
+};
---
+++ src/config/mail.example.ts
@@ -0,0 +1,12 @@
+class EmailConfig {
+  public host = 'mail.host.com';
+  public port = -1;
+  public auth = {
+    user: 'example@domain.com',
+    pass: '',
+  };
+  public secure = false;
+  public tls = { rejectUnauthorized: false };
+}
+
+export default new EmailConfig();
