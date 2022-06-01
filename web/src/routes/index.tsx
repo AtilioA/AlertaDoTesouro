367be584f26520c61e6b47887a357a08de1f6bc0
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-06-01 00:53:26 -0300

    Tá atualizado amigo toma

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,41 +1,35 @@
+import { useContext } from 'react';
+import { Outlet, Navigate } from 'react-router';
 import { Routes, Route } from 'react-router-dom';
 
-import PrivateRoute from './PrivateRoute';
 import Dashboard from '../pages/Dashboard';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
 import Account from '../pages/Account';
 import Notifications from '../pages/Notifications';
 import ToS from '../pages/ToS';
+import { AuthContext } from '../context/AuthContext';
 
-const routes = [
-  {
-    path: '/',
-    element: <Dashboard />,
-    children: [
-      { path: 'dashboard', element: <Dashboard /> },
-      { path: 'registrar', element: <SignUp /> },
-      { path: 'login', element: <SignIn /> },
-      {
-        path: 'conta',
-        element: (
-          <PrivateRoute>
-            <Account />
-          </PrivateRoute>
-        ),
-      },
-      { path: 'notificacoes', element: <Notifications /> },
-      { path: 'tos', element: <ToS /> },
-    ],
-  },
-];
+function PrivateRoute() {
+  const { user } = useContext(AuthContext);
+
+  // If autenticated context is present
+  return user ? <Outlet /> : <Navigate to="/login" />;
+}
 
 export default function AppRoutes() {
   return (
     <Routes>
-      {routes.map(props => (
-        <Route {...props} path={props.path} key={props.path} />
-      ))}
+      <Route path="/" element={<Dashboard />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="registrar" element={<SignUp />} />
+        <Route path="login" element={<SignIn />} />
+        <Route path="conta" element={<PrivateRoute />}>
+          <Route path="" element={<Account />} />
+        </Route>
+        <Route path="notificacoes" element={<Notifications />} />
+        <Route path="tos" element={<ToS />} />
+      </Route>
     </Routes>
   );
 }
