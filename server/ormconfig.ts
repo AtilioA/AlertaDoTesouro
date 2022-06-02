fd016e3ab46ef81f97f57c675fcfdcde25e8b30d
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-06-02 00:15:09 -0300

    feat(server): Using loadRequiredEnv on ormconfig

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
+import loadRequiredEnv from './src/utils/loadRequiredEnv';
 
 dotenv.config();
 
 export = {
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  type: loadRequiredEnv('DB_TYPE'),
+  host: loadRequiredEnv('DB_HOST'),
+  port: loadRequiredEnv('DB_PORT'),
+  username: loadRequiredEnv('DB_USERNAME'),
+  password: loadRequiredEnv('DB_PASSWORD'),
+  database: loadRequiredEnv('DB_DATABASE'),
   entities: ['./src/models/*.ts'],
   migrations: ['./src/database/migrations/*.ts'],
   cli: {
