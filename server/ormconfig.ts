bf35d1688d7b19588848c4483168446bda924813
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-06-09 02:02:09 -0300

    fix(server): Properly using env

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,19 +1,44 @@
 import dotenv from 'dotenv';
+import { ConnectionOptions } from 'typeorm';
 import loadRequiredEnv from './src/utils/loadRequiredEnv';
 
 dotenv.config();
+const testEnv = dotenv.config({ path: '.env.test' });
+if (testEnv.error) {
+  throw testEnv.error;
+}
 
-export = {
-  type: loadRequiredEnv('DB_TYPE'),
-  host: loadRequiredEnv('DB_HOST'),
-  port: loadRequiredEnv('DB_PORT'),
-  username: loadRequiredEnv('DB_USERNAME'),
-  password: loadRequiredEnv('DB_PASSWORD'),
-  database: loadRequiredEnv('DB_DATABASE'),
-  entities: ['./src/models/*.ts'],
-  migrations: ['./src/database/migrations/*.ts'],
-  cli: {
-    migrationsDir: './src/database/migrations',
+const connectionOptions: ConnectionOptions[] = [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: loadRequiredEnv('DB_HOST'),
+    port: Number(loadRequiredEnv('DB_PORT')),
+    username: loadRequiredEnv('POSTGRES_USER'),
+    password: loadRequiredEnv('POSTGRES_PASSWORD'),
+    database: loadRequiredEnv('POSTGRES_DB'),
+    entities: ['./src/models/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
   },
-  migrationsTransactionMode: 'each',
-};
+  {
+    name: 'test',
+    type: 'postgres',
+    host: loadRequiredEnv('DB_HOST_TEST'),
+    port: Number(loadRequiredEnv('DB_PORT_TEST')),
+    username: testEnv.parsed?.POSTGRES_USER,
+    password: testEnv.parsed?.POSTGRES_PASSWORD,
+    database: testEnv.parsed?.POSTGRES_DB,
+    entities: ['./src/models/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+    dropSchema: true,
+    // logging: false,
+  },
+];
+
+export = connectionOptions;
