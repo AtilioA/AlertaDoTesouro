6abbd5c2ba5557d8888bbce2a3dd37b8386968dd
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-08-10 01:01:23 -0300

    feat(web): show controls according to current state
    Signed-off-by: Henriquelay <37563861+Henriquelay@users.noreply.github.com>

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
+import { User } from '../../@types/global';
 import { Container, Profile } from './styles';
 
 export default function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
+    localStorage.getItem('@AlertaDoTesouro:token') !== null,
+  );
+  const [mailAddress, setMailAddress] = useState<string>('');
+
+  useEffect(() => {
+    const loggedIn = localStorage.getItem('@AlertaDoTesouro:token') !== null;
+    setIsLoggedIn(loggedIn);
+    if (loggedIn) {
+      const mail = localStorage.getItem('@AlertaDoTesouro:user');
+      if (!mail) {
+        setMailAddress('user');
+      } else {
+        setMailAddress((JSON.parse(mail) as User).email);
+      }
+    }
+  }, [localStorage.getItem('@AlertaDoTesouro:token')]);
+
   return (
     <Container>
       <nav>
@@ -11,15 +31,18 @@ export default function Header() {
       <aside>
         <Profile>
           <a href="/dashboard">Dashboard</a>
-
-          <a href="/notificacoes">Notificações</a>
-
-          <a href="/conta">Conta</a>
         </Profile>
 
-        <a href="/login">
-          <button type="submit">Logar</button>
-        </a>
+        {isLoggedIn && mailAddress ? (
+          <>
+            <a href="/notificacoes">Notificações</a>
+            <a href="/conta">{mailAddress}</a>
+          </>
+        ) : (
+          <a href="/login">
+            <button type="submit">Logar</button>
+          </a>
+        )}
       </aside>
     </Container>
   );
