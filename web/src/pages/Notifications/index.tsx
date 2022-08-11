ad611788a1ccbeb839e5967c6a202c5f8854c558
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-08-10 23:12:48 -0300

    ⚗️👨‍🔬 last quick fixes
    Signed-off-by: Henriquelay <37563861+Henriquelay@users.noreply.github.com>

--- web/src/pages/Notifications/index.tsx
+++ web/src/pages/Notifications/index.tsx
@@ -11,8 +11,76 @@ import {
 // import Input from '../../components/Input';
 
 export default function Notifications() {
-  function handleNotifyChange() {
-    return true;
+  const [notifications, setNotifications] = useState<NotificationType[]>([]);
+  const [user, setUser] = useState<UserType>(
+    JSON.parse(localStorage.getItem('@AlertaDoTesouro:user') ?? '') as UserType,
+  );
+
+  useEffect(() => {
+    console.log(user.notify, user.notifyByEmail, user.notifyByBrowser);
+    api
+      .put(`/users/`, {
+        notify: user.notify,
+        notifyByEmail: user.notifyByEmail,
+        notifyByBrowser: user.notifyByBrowser,
+      })
+      .then(() => {
+        localStorage.setItem('@AlertaDoTesouro:user', JSON.stringify(user));
+        console.log('Notification status updated!');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [user, user.notify, user.notifyByEmail, user.notifyByBrowser]);
+
+  useEffect(() => {
+    api
+      .get(`/users/`)
+      .then(userResponse => {
+        const userData = userResponse.data as UserType;
+        setUser(userData);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    api
+      .get('/notifications')
+      .then(notificationsResponse => {
+        const responseData = notificationsResponse.data as NotificationType[];
+
+        setNotifications(responseData);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  async function handleNotificationDelete(notificationID: string) {
+    // Fools the user and might not be needed if the notifications are getting deleted from the backend
+    const newNotifications = notifications.filter(
+      mapNotification => mapNotification.id !== notificationID,
+    );
+    setNotifications([...newNotifications]);
+
+    // DELETE request + bearer token to notification endpoint with notification ID
+    try {
+      await api.delete(`/notifications/${notificationID}`);
+      console.log('Notification deleted');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log('USER', user);
+
+  function formatDate(date: string) {
+    const dateObject = new Date(date);
+    const dateFormatted = `${dateObject.getDate()}/${
+      dateObject.getMonth() + 1
+    }/${dateObject.getFullYear()}`;
+
+    return dateFormatted;
   }
 
   return (
@@ -26,26 +94,63 @@ export default function Notifications() {
           <div id="toggle-with-label">
             <span>Receber notificações</span>
             <Toggle
-              id="notification-status"
-              defaultChecked
-              onChange={() => handleNotifyChange()}
+              id="notification-status-global-all"
+              // defaultChecked={user.notify}
+              checked={user.notify}
+              onChange={() => {
+                // Has to be set inside here because the function is execute in the context before state reaction
+                const futureNotify = !user.notify;
+                setUser(prev => ({ ...prev, notify: futureNotify }));
+                if (!futureNotify) {
+                  setUser(prev => ({
+                    ...prev,
+                    notify: false,
+                    notifyByBrowser: false,
+                    notifyByEmail: false,
+                  }));
+                }
+              }}
             />
           </div>
           <div id="global-notification-settings-body">
             <div id="toggle-with-label">
               <span>Receber notificações por e-mail</span>
               <Toggle
-                id="notification-status"
-                defaultChecked
-                onChange={() => handleNotifyChange()}
+                id="notification-status-global-email"
+                defaultChecked={user.notifyByEmail}
+                checked={user.notifyByEmail}
+                onChange={() => {
+                  // Has to be set inside here because the function is execute in the context before state reaction
+                  const futureNotify = !user.notifyByEmail;
+                  setUser(prev => ({ ...prev, notifyByEmail: futureNotify }));
+                  if (futureNotify) {
+                    setUser(prev => ({
+                      ...prev,
+                      notify: true,
+                      notifyByEmail: true,
+                    }));
+                  }
+                }}
               />
             </div>
             <div id="toggle-with-label">
               <span>Receber notificações pelo navegador</span>
               <Toggle
-                id="notification-status"
-                defaultChecked
-                onChange={() => handleNotifyChange()}
+                id="notification-status-global-browser"
+                defaultChecked={user.notifyByBrowser}
+                checked={user.notifyByBrowser}
+                onChange={() => {
+                  // Has to be set inside here because the function is execute in the context before state reaction
+                  const futureNotify = !user.notifyByBrowser;
+                  setUser(prev => ({ ...prev, notifyByBrowser: futureNotify }));
+                  if (futureNotify) {
+                    setUser(prev => ({
+                      ...prev,
+                      notify: true,
+                      notifyByBrowser: true,
+                    }));
+                  }
+                }}
               />
             </div>
           </div>
